a2425b8e19236ba2afcf112631d1f44d41e0b49e
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2021-10-24 13:41:29 +0200

    Update ipax.py
    Added support for outfile parameter, such as multiple parameter to parse all ipa files in a folder

--- appdata/ipax.py
+++ appdata/ipax.py
@@ -58,60 +58,108 @@ def extractAppIcon(IPAfile, IconNames):
 def uncrushPng(png_path):
     os.system('python2 /appdata/ipin.py ' + png_path)
 
+def processIPA(ipaFullPath):
+    fileName = os.path.basename(ipaFullPath)
+    baseFileName = os.path.splitext(fileName)[0]
+    MatchPattern = "Payload\/.+?..app/Info.plist"
+    with ZipFile(ipaFullPath, 'r') as zipObj:
+        listOfFileNames = zipObj.namelist()
+        for fileName in listOfFileNames:
+            m = re.match(MatchPattern, fileName)
+            if m:
+                zipObj.filename = os.path.basename(zipObj.filename)
+                zipObj.extract(fileName, '/appdata/tmp_infoplist')
+                os.rename('/appdata/' + 'tmp_infoplist/' + fileName, '/home/work/Info.plist')
+    if not os.path.isfile('/home/work/Info.plist'):
+        return 'NotAnIPAFile'
+    else:
+        infile = open('/home/work/Info.plist', 'rb')
+        firstLine = infile.readline()
+        if (firstLine.startswith(b"bplist")):
+            os.system("plistutil -i " + "/home/work/Info.plist -o /home/work/Info_.plist")
+            os.remove("/home/work/Info.plist")
+            os.rename("/home/work/Info_.plist", "/home/work/Info.plist")
+        xml = open('/home/work/Info.plist','r').read()
+
+        appName = grabAppleXMLValue('CFBundleName', xml)
+        appVersion = grabAppleXMLValue('CFBundleShortVersionString', xml)
+        appBundleIdentifier = grabAppleXMLValue('CFBundleIdentifier', xml)
+
+        IconNames = grabAppleIconNames(xml)
+        extractAppIcon(ipaFullPath, IconNames)
+        uncrushPng('/home/work/AppIcon.png')
+
+        genericName = os.path.splitext(baseFileName)[0] + '.png'
+        shutil.move('/home/work/AppIcon.png','/home/work/files/' + genericName)
+
+        result = dict()
+        result["AppName"] = appName
+        result["AppVersion"] = appVersion
+        result["AppBundleIdentifier"] = appBundleIdentifier
+        result["IconName"] = genericName
+        return result
+
+ipaLocation = "/home/work/files/"
 
 ipaFilename = sys.argv[1]
-ipaLocation = "/home/work/files/"
+if sys.argv[2] != "none":
+    outfile = ipaLocation + sys.argv[2]
+else:
+    outfile = sys.argv[2]
+multiple = sys.argv[3]
 
 ipaFullPath = ipaLocation + ipaFilename
 
-if sys.argv[1] == "none":
-    print("Run this image with following arguments:\n")
-    print('-e filename="sample.ipa" --rm -v /tmp/ipas/:/home/work/files\n')
-    print("Replace sample.ipa with the target filename.")
-    print("Replace /tmp/ipas/ with the location of the IPA file.")
-    exit()
-
-if not os.path.isfile(ipaFullPath):
-    print('{"Error": "FileNotFound"}')
-    exit()
-
-MatchPattern = "Payload\/.+?..app/Info.plist"
-with ZipFile(ipaFullPath, 'r') as zipObj:
-    listOfFileNames = zipObj.namelist()
-    for fileName in listOfFileNames:
-        m = re.match(MatchPattern, fileName)
-        if m:
-            zipObj.filename = os.path.basename(zipObj.filename)
-            zipObj.extract(fileName, '/appdata/tmp_infoplist')
-            os.rename('/appdata/' + 'tmp_infoplist/' + fileName, '/home/work/Info.plist')
-
-if not os.path.isfile('/home/work/Info.plist'):
-    print('{"Error": "NotAnIPAFile"}')
+if multiple == "true":
+        allFiles = os.listdir(ipaLocation)
+        json_contents = []
+        for f in allFiles:
+            if f.lower().endswith(".ipa"):
+                ipaFullPath = ipaLocation + f
+                returnData = processIPA(ipaFullPath)
+                if (returnData == "NotAnIPAFile"):
+                    print("Warning: file " + f + " is not an ipa file.")
+                    continue
+                json_contents.append(returnData)
+        if outfile == "none":
+            print(json.dumps(json_contents))
+        else:
+            try:
+                try:
+                    os.remove(outfile)
+                except:
+                    pass
+                outF = open(outfile, "a")
+                outF.write(json.dumps(json_contents))
+            except OSError as err:
+                print("Couldn't save to outfile: {0}".format(err))
+                pass
 else:
-    infile = open('/home/work/Info.plist', 'rb')
-    firstLine = infile.readline()
-    if (firstLine.startswith(b"bplist")):
-        os.system("plistutil -i " + "/home/work/Info.plist -o /home/work/Info_.plist")
-        os.remove("/home/work/Info.plist")
-        os.rename("/home/work/Info_.plist", "/home/work/Info.plist")
-    xml = open('/home/work/Info.plist','r').read()
+    if sys.argv[1] == "none":
+        print("Run this image with following arguments:\n")
+        print('-e filename="sample.ipa" --rm -v /tmp/ipas/:/home/work/files\n')
+        print("Replace sample.ipa with the target filename.")
+        print("Replace /tmp/ipas/ with the location of the IPA file.\n")
+        print("Alternatively you can process all ipa files like this:")
+        print('-e multiple="true" --rm -v /tmp/ipas/:/home/work/files\n\n')
+        print("You can output the JSON contents to a text file by using:\n")
+        print('-e outfile="result.json"')
+        exit()
+    else:
+        if not os.path.isfile(ipaFullPath):
+            print('{"Error": "FileNotFound"}')
+            exit()
 
-    appName = grabAppleXMLValue('CFBundleName', xml)
-    appVersion = grabAppleXMLValue('CFBundleShortVersionString', xml)
-    appBundleIdentifier = grabAppleXMLValue('CFBundleIdentifier', xml)
-
-    IconNames = grabAppleIconNames(xml)
-    extractAppIcon(ipaFullPath, IconNames)
-    uncrushPng('/home/work/AppIcon.png')
-
-    genericName = os.path.splitext(ipaFilename)[0] + '.png'
-    shutil.move('/home/work/AppIcon.png','/home/work/files/' + genericName)
-
-    result = dict()
-    result["AppName"] = appName
-    result["AppVersion"] = appVersion
-    result["AppBundleIdentifier"] = appBundleIdentifier
-    result["IconName"] = genericName
-
-    json_result = json.dumps(result)
-    print(json_result)
+        if outfile == "none":
+            print(json.dumps(processIPA(ipaFullPath)))
+        else:
+            try:
+                try:
+                    os.remove(outfile)
+                except:
+                    pass
+                outF = open(outfile, "a")
+                outF.write(json.dumps(processIPA(ipaFullPath)))
+            except OSError as err:
+                print("Couldn't save to outfile: {0}".format(err))
+                pass
